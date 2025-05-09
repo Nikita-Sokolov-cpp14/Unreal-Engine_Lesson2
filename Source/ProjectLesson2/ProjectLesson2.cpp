0553bbb21390f982a903796aebfa83e3c8ccbd42
Nikita Sokolov <nekitsok10@yandex.ru>
2025-05-09 16:19:40 +0300

    initial commit

---
+++ Source/ProjectLesson2/ProjectLesson2.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectLesson2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectLesson2, "ProjectLesson2" );
